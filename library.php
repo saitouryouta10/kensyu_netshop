881663fa432ca666df8c7f5694907f86c8f6e62a
saitouryouta10 <saito@se-sendai.co.jp>
2022-06-10 16:21:37 +0900

    library.phpの追加

---
+++ library.php
@@ -0,0 +1,13 @@
+<?php
+function h($value){
+    return htmlspecialchars($value,ENT_QUOTES);
+}
+
+function dbconnect(){
+    $db = new mysqli("localhost:8889","root","root","kensyu_db");
+    if(!$db){
+		die($db->error);
+	}
+    return $db;
+}
+?>
