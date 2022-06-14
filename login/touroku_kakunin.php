3fec8d2712e14b77f88173f7953086bdea7f2891
saitouryouta10 <saito@se-sendai.co.jp>
2022-06-14 16:10:53 +0900

    新規会員登録　50%

---
+++ login/touroku_kakunin.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+var_dump($_SESSION["form"]);
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOGEHOGE SHOP</title>
+</head>
+<body>
+    
+</body>
+</html>
