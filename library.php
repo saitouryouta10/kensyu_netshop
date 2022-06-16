e9c90722212971a1b347d0a0312d5f17cd273c21
takahashi-ses <takahashi@se-sendai.co.jp>
2022-06-16 13:59:05 +0900

    header, footer functionを追記

--- library.php
+++ library.php
@@ -11,4 +11,23 @@ function dbconnect(){
     return $db;
 }
 
+function header_inc() {
+    if(isset($_SESSION['id'])){
+        //ログインしてる人用ヘッダー
+        include(dirname(__FILE__) . "./head/header_logout.php");
+    }else{
+        //ログインしてない人用ヘッダー
+        include(dirname(__FILE__) . "./head/header_login.php");
+    }
+}
+
+function footer_inc() {
+    if(isset($_SESSION['id'])){
+        //ログアウトしてる人用フッター
+        include(dirname(__FILE__) . "./head/footer_logout.php");
+    }else{
+        //ログインしてない人用フッター
+        include(dirname(__FILE__) . "./head/footer_login.php");
+    }
+}
 ?>
