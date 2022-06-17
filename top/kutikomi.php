182bd54315910de89d7c9506111b38c35b8d70bc
kumases <105776488+kumases@users.noreply.github.com>
2022-06-17 15:40:00 +0900

    口コミ機能実装

--- top/kutikomi.php
+++ top/kutikomi.php
@@ -2,6 +2,8 @@
 session_start();
 require('../library.php');
 
+$item_id=$_GET['id'];
+
 if(isset($_SESSION['id']) && isset($_SESSION['name'])){
     $userid =$_SESSION['id'];
     $name = $_SESSION['name'];
@@ -10,24 +12,33 @@ if(isset($_SESSION['id']) && isset($_SESSION['name'])){
     exit();
 }
 
-if(isset($_SESSION['star'])){
-  $star=$_SESSION['star'];
-}
+
 $db =dbconnect();
 
 //メッセージの投稿
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
+  if(isset($_POST['com_id'])==true){
+    $com_id=$_POST['com_id'];
+    //  echo $com_id;
+    $sqls='delete from reviews where id=?';
+    $stmts =$db ->prepare($sqls);
+    $stmts->bind_Param("i",$com_id);
+    $stmts->execute();
+
+    }else{
     $comment = filter_input(INPUT_POST,'comment',FILTER_SANITIZE_STRING);
+    $star = filter_input(INPUT_POST,'star',FILTER_SANITIZE_STRING);
 
-    $stmt = $db->prepare('insert into reviews (comment, user_id,star) values(?,?,?)');
+    $stmt = $db->prepare('insert into reviews (comment,user_id,star,itm_id) values(?,?,?,?)');
 
-    $stmt->bind_param('sii',$comment,$userid,$star);
+    $stmt->bind_param('siii',$comment,$userid,$star,$item_id);
     $succsess = $stmt->execute();
     if(!$succsess){
         die($db->error);
     }
-    header('Location: index.php');
+    header('Location: kutikomi.php?id='.$item_id.'');
     exit();
+  }
 }
 
 ?>
@@ -40,17 +51,22 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ひとこと掲示板</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css"/>
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
+  <link rel="stylesheet" type="text/css" href="./css/styles.css">
+  <link rel="stylesheet" type="text/css" href="../css/style.css">
 </head>
 
 <body>
+  <a href="top.php">
+    <h1 class="title_name">HOGEHOGE SHOP</h1>
+  </a>
 <div id="wrap">
     <div id="head">
         <h1>口コミ</h1>
     </div>
     <div id="content">
         <div style="text-align: right"><a href="logout.php">ログアウト</a></div>
+
+
         <form action="" method="post">
             <dl>
                 <dt><?php echo h($name); ?>さん、メッセージをどうぞ</dt>
@@ -60,38 +76,52 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
             </dl>
             <select name="star" id="">
               <?php for($i=1;$i<6;$i++): ?>
-                <option value="<?php $i ?>"></option>
+                <option value="<?php echo $i ?>"><?php echo $i; ?></option>
+                <?php endfor ; ?>
             </select>
             <div>
                 <p>
-                    <input type="submit" value="投稿する"/>
+                    <button type="submit"class="btn btn-success">投稿する</button>
+                    <!-- <input type="submit" value="投稿する"/> -->
                 </p>
             </div>
         </form>
-        <?php
-        $stmt= $db->prepare('select r.comment,r.star,r.created from reviews r');
-        if(!$stmt){
-            die($db->error);
-        }
-        $succsess = $stmt->execute();
-        if(!$succsess){
-            die($db->error);
-        }
 
-        $stmt->bind_result($comment,$star,$created);
-        while($stmt->fetch()): ?>
+        <?php
+        $stmt1= $db->query('select r.comment,r.star,r.created from reviews r where itm_id='.$item_id.'');
+        $sql3='select id from reviews where itm_id='.$item_id.'';
+        $stmt3=$db->query($sql3);
+        // if(!$stmt){
+        //     die($db->error);
+        // }
+        // $succsess = $stmt->execute();
+        // if(!$succsess){
+        //     die($db->error);
+        // }
+
+        while($result2=$stmt1->fetch_assoc()):
+        // print_r($result2);
+        ?>
+            <?php $result3 = $stmt3->fetch_assoc(); ?>
+
         <div class="msg">
-            <?php if($comment): ?>
-            <img src="member_picture/<?php echo h($comment); ?>" width="48" height="48" alt=""/>
-            <?php endif ;?>
-            <p><?php echo h($comment); ?><span class="name">（<?php echo h($name); ?>）</span></p>
-            <p class="day"><a href="view.php?id=<?php echo h($id); ?>"><?php echo h($created) ; ?></a>
+            <p><?php echo h($result2['comment']); ?><br><span class="name"><?php echo h($name); ?></span></p>
+            <p>評価<?php echo h($result2['star']); ?></p>
+            <p class="day"><a href="view.php?id=<?php echo h($id); ?>"><?php echo h($result2['created']) ; ?></a>
             <?php if($_SESSION['id'] === $userid): ?>
-                [<a href="delete.php?id=<?php echo h($id); ?>" style="color: #F33;">削除</a>]
+
+              <form action="" method="POST">
+              <input type="hidden" name="com_id" value="<?php echo $result3['id']; ?>">
+              <?php echo $result3['id']; ?>
+              <button type="submit"class="btn btn-danger">削除</button>
+                <!-- <input type="submit" value="削除"/> -->
+              </form>
+
                 <?php endif; ?>
             </p>
         </div>
-        <?php endwhile ; ?>
+        <?php endwhile; ?>
+
     </div>
 </div>
 </body>
--- top/shouhin_shousai.php
+++ top/shouhin_shousai.php
@@ -9,6 +9,9 @@ $login=1;
 if(isset($_SESSION["id"])){
   //セッション情報がある場合は普通に画面遷移
   $userid=$_SESSION['id'];
+  if(isset($_SESSION['name'])){
+  $name = $_SESSION['name'];
+  }
 }else{
 
     //セッション情報がなかったらログイン画面に遷移してログイン画面でログインしろ！的なエラーメッセージ出しときます
@@ -41,6 +44,32 @@ if(isset($_POST['cartin_button'])==true){
 
 }
 
+//メッセージの投稿
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+  if(isset($_POST['com_id'])==true){
+    $com_id=$_POST['com_id'];
+    //  echo $com_id;
+    $sqls='delete from reviews where id=?';
+    $stmts =$db ->prepare($sqls);
+    $stmts->bind_Param("i",$com_id);
+    $stmts->execute();
+
+    }elseif(isset($_POST['star'])==true){
+    $comment = filter_input(INPUT_POST,'comment',FILTER_SANITIZE_STRING);
+    $star = filter_input(INPUT_POST,'star',FILTER_SANITIZE_STRING);
+
+    $stmt = $db->prepare('insert into reviews (comment,user_id,star,itm_id) values(?,?,?,?)');
+
+    $stmt->bind_param('siii',$comment,$userid,$star,$item_id);
+    $succsess = $stmt->execute();
+    if(!$succsess){
+        die($db->error);
+    }
+    header('Location: ');
+    exit();
+  }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -60,40 +89,45 @@ if(isset($_POST['cartin_button'])==true){
   </a>
 
 <?php
-$sql = 'select * from items where id=?';
-$stmt =$db ->prepare($sql);
-$stmt->bind_Param("s",$item_id);
-$stmt->execute();
+$sql = 'select * from items where id='.$item_id.'';
 
-$stmt->bind_result($id,$name,$price,$jenre,$stock,$item_link,$setumei,$syousai,$picture,$created);
-if($stmt->fetch()): ?>
+$stmt =$db ->query($sql);
+// $stmt->bind_Param("s",$item_id);
+// $stmt->execute();
+
+// $stmt->bind_result($id,$item_name,$price,$jenre,$stock,$item_link,$setumei,$syousai,$picture,$created);
+
+if($rec=$stmt->fetch_assoc()):
+
+// print_r($rec);
+
+//if($stmt->fetch()): ?>
 
 <div class="s_main">
   <div class="shouhin_img">
-    <?php if($picture): ?>
-        <img src="./img/<?php echo h($picture); ?>" >
+    <?php if($rec['picture']): ?>
+        <img src="./img/<?php echo h($rec['picture']); ?>" >
         <?php else: ?>
           <img src="./img/noimage.png" >
           <?php endif ;?>
         </div>
      <div>
-      <p><?php echo h($name); ?><span class="name"></p>
-      <p><?php echo h($price); ?>円</span></p>
+      <p><?php echo h($rec['name']); ?><span class="name"></p>
+      <p><?php echo h($rec['price']); ?>円</span></p>
 
-      <?php if($stock >0): ?>
+      <?php if($rec['stock'] >0): ?>
+
+
          <form action="" method="POST">
           <select name="kazuerabi" >
-           <?php for($i=1; $i<=$stock;$i++):?>
+           <?php for($i=1; $i<=$rec['stock'];$i++):?>
            <option value="<?php echo $i; ?>"><? echo $i; ?>個</option>
            <?php endfor ;?>
           </select>
          <button type="submit"name="cartin_button" class="btn btn-success">カートに入れる</button>
-
-
-
-          <!-- $stmt->bind_result($id,$name,$price,$jenre,$stock,$item_link,$setumei,$syousai,$picture,$created); -->
-
         </form>
+
+
      <? else:?>
        <p style="color:red;">在庫がありません</p>
        <?php endif; ?>
@@ -108,15 +142,59 @@ if($stmt->fetch()): ?>
       </div>
 
     </div>
-    <p ><?php echo h($setumei) ; ?></p>
-    <?php else : ?>
-      <p>その商品ページは削除されたか、URLが間違えています</p>
-      <?php endif; ?>
+    <p ><?php echo h($rec['setumei']) ; ?></p>
 
-</div>
+  </div>
 
-<div>
+  <div>
 
-</div>
-</body>
-</html>
+<br>
+<p>レビュー<button type="button" onclick="location.href='kutikomi.php?id=<?php echo $item_id;?>';" class="btn btn-success">一覧</button></p>
+<br>
+<?php
+
+          $stmt= $db->prepare('select r.comment,r.star,r.created from reviews r where itm_id='.$item_id.'');
+          $sql3='select id from reviews where user_id='.$userid.'';
+          $stmt3=$db->query($sql3);
+          if(!$stmt){
+              die($db->error);
+          }
+          $succsess = $stmt->execute();
+          if(!$succsess){
+              die($db->error);
+          }
+
+          $stmt->bind_result($comment,$star,$created);
+          while($stmt->fetch()): ?>
+              <?php $result3 = $stmt3->fetch_assoc(); ?>
+
+          <div class="msg">
+              <p><?php echo h($comment); ?><span class="name">（<?php echo h($name); ?>）</span></p>
+              <p>評価<?php echo h($star); ?></p>
+              <p class="day"><a href="view.php?id=<?php echo h($id); ?>"><?php echo h($created) ; ?></a>
+              <?php if($_SESSION['id'] === $userid): ?>
+
+
+
+                <form action="" method="POST">
+                <input type="hidden" name="com_id" value="<?php echo $result3['id']; ?>">
+                <?php echo $result3['id']; ?>
+                <button type="submit"class="btn btn-danger">削除</button>
+                  <!-- <input type="submit" value="削除"/> -->
+                </form>
+
+                  <?php endif; ?>
+              </p>
+          </div>
+          <?php endwhile ; ?>
+      </div>
+
+
+
+      <?php  else : ?>
+        <p>その商品ページは削除されたか、URLが間違えています</p>
+        <?php endif; ?>
+    </div>
+
+  </body>
+  </html>
