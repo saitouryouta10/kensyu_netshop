21183b98d4ad8b00aef511c2850b255e87794953
saitouryouta10 <saito@se-sendai.co.jp>
2022-06-20 16:33:25 +0900

    リンクの追加

--- head/footer_logout.php
+++ head/footer_logout.php
@@ -13,17 +13,17 @@
 <body>
     <footer>
             <div class="footer_list">
-                <ul class="list_1">
-                    <li><a href="#">ログアウト</a></li>
-                    <li><a href="./top.php">トップページへ</a></li>
+            <ul class="list_1">
+                    <li><a href="../login/logout.php">ログアウト</a></li>
+                    <li><a href="../top/top.php">トップページへ</a></li>
                 </ul>
                 <ul class="list_2">
-                    <li><a href="#">お問い合わせ</a></li>
-                    <li><a href="#">会員情報</a></li>
+                    <li><a href="../inq/toiawase.php">お問い合わせ</a></li>
+                    <li><a href="../kaiinjouhou/kaiin_jouhou.php">会員情報</a></li>
                 </ul>
                 <ul class="list_3">
                     <li><a href="#">お気に入り</a></li>
-                    <li><a href="./cart.php">カート</a></li>
+                    <li><a href="../top/cart.php">カート</a></li>
                 </ul>
             </div>
             <p class="copyright">© 2022 Kensyu_netshop<p>
