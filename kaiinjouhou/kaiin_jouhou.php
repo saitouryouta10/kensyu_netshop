ac5a1b117af1b1103c48c8a45baeef2dbc26d87e
kumases <105776488+kumases@users.noreply.github.com>
2022-06-21 11:05:19 +0900

    修正

--- kaiinjouhou/kaiin_jouhou.php
+++ kaiinjouhou/kaiin_jouhou.php
@@ -2,16 +2,11 @@
 session_start();
 require('../library.php');
 
-<<<<<<< HEAD
+
 // ログインしているか確認
 if (isset($_SESSION['id']) && isset($_SESSION['nickname'])) {
     $id = $_SESSION['id'];
-=======
-//ログインしているか確認
-if (isset($_SESSION['id']) && isset($_SESSION['nickname'])) {
-    $id = $_SESSION['id'];
 
->>>>>>> af86535 (修正)
     $name = $_SESSION['nickname'];
 } else {
     $login = 1;
