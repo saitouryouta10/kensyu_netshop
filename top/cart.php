50c2005e8b39931e04af1f439ef4da5c1748c9ef
kumases <105776488+kumases@users.noreply.github.com>
2022-06-21 11:01:07 +0900

    修正

--- top/cart.php
+++ top/cart.php
@@ -64,7 +64,7 @@ $total=0;
   <title>Document</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="./css/styles.css">
-  <link rel="stylesheet" type="text/css" href="../css/style.css">
+  <link rel="stylesheet" type="text/css" href="../style.css">
 </head>
 <body>
   <header>
