3c8d7f2945cc8eecee2c36cb96f376f3c11aafb5
kumases <105776488+kumases@users.noreply.github.com>
2022-06-21 13:54:30 +0900

    修正

--- top/shouhin_shousai.php
+++ top/shouhin_shousai.php
@@ -72,9 +72,10 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
   <?php header_inc(); ?>
 </header>
 <main>
-  <!-- <a href="top.php">
-    <h1 class="title_name">HOGEHOGE SHOP</h1>
-  </a> -->
+<div class="btn-modoru-rireki">
+    <a class="btn btn-outline-secondary btn-block" href="../top/top.php">トップに戻る</a>
+</div>
+
 
 <?php
 $sql = 'select * from items where id='.$item_id.'';
@@ -139,24 +140,16 @@ if($rec=$stmt->fetch_assoc()):
 
         <form action="" method="POST">
         <div class="shousai_b">
-         <button type="submit"name="favorite_button" class="btn btn-success">お気に入りに追加</button>
+         <button type="submit"name="favorite_button" class="btn btn-outline-warning">お気に入りに追加</button>
         </form>
         <p style="color:pink; margin-top:0">
           <?php
             // お気に入りに追加ボタンが押されたとき,favoriteデータベースに追加
             if(isset($_POST['favorite_button'])==true){
               $sql3 = 'insert into favorite(user_id,item_id) values('.$userid.','.$item_id.')';
-<<<<<<< HEAD
-<<<<<<< HEAD
-              $sql_3='select count(*) from favorite where item_id='.$item_id.' and user_id='.$userid.'';
-=======
-              $sql_3='select count(*) from cart where item_id='.$item_id.' and user_id='.$userid.'';
->>>>>>> af86535 (修正)
-=======
 
               $sql_3='select count(*) from favorite where item_id='.$item_id.' and user_id='.$userid.'';
 
->>>>>>> f06340e (修正)
               $stmt_3=$db->query($sql_3);
               $rec3=$stmt_3->fetch_assoc();
               if($rec3['count(*)']==0 ){
